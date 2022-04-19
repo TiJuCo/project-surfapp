effe03f7ba2ea62868f38c9029e60f7b27692cb0
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-04-19 17:40:23 +0100

    tested storm glass API

--- src/App.js
+++ src/App.js
@@ -5,21 +5,42 @@ import axios from "axios";
 import data from "./components/data";
 
 function App() {
-  const [temperature, setTemperature] = useState([1, 2, 3]);
-
   const getTemperature = () => {
+    const lat = 38.6782248145406;
+    const lng = -9.338068004467669;
+    const params = [
+      "windSpeed",
+      "waterTemperature",
+      "seaLevel",
+      "currentDirection",
+      "gust",
+      "humidity",
+      "precipitation",
+      "swellPeriod",
+      "secondarySwellPeriod",
+      "wavePeriod",
+      "windWavePeriod",
+      "windDirection",
+    ];
+
     axios
       .get(
-        "https://api.ipma.pt/open-data/forecast/oceanography/daily/hp-daily-sea-forecast-day0.json"
+        `https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${lng}&start=2019-03-15&end=2019-03-15`,
+        {
+          headers: {
+            Authorization:
+              "f71f766c-b4e5-11ec-b2a7-0242ac130002-f71f76e4-b4e5-11ec-b2a7-0242ac130002",
+          },
+        }
       )
-      .then((response) => response.data)
-      .then((data) => {
-        setTemperature(data.data);
+      .then((response) => response)
+      .then((jsonData) => {
+        console.log(jsonData);
       });
   };
   return (
     <div className="App">
-      <Temperature temperature={temperature} data={data[0]} />
+      {/* <Temperature temperature={temperature} data={data[0]} /> */}
       <button onClick={getTemperature}>Check the weather</button>
     </div>
   );
