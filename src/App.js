aec9c711a46983ea73cd6cceb9e455b26f893fa8
franciscocpereira <fmcp31@gmail.com>
2022-04-20 17:05:35 +0100

    reset Develop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <div className="App">
+      </div>
   );
 }
 
