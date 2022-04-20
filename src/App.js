250408c40f04ceaecdc1a90db68c023b96530550
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-04-20 14:54:12 +0100

    Created the weather forecast component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import WeatherForecast from "./components/WeatherForecast/WeatherForecast";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>COMPONENT ONE</h1>
+      <h1>COMPONENT TWO</h1>
+      <h1>COMPONENT THREE</h1>
+      <WeatherForecast />
     </div>
   );
 }
