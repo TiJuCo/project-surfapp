02a4b7f56c79d2f6fa22b5ce3ba90ad36207f004
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-04-22 10:06:29 +0100

    final final changes to merge

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import NewsLatest from "../news/News-Latest";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import WeatherForecast from "../WeatherForecast/WeatherForecast";
-import '../../styles/styles.css'
-import './home.css'
+import "../../styles/styles.css";
+import "./home.css";
+import NewsSlider from "../news/News-Latest-Mobile";
 
 function Home() {
   return (
-    <div className="content" style={{minHeight: `100vh;`}}>
-      {/* <div className="container">
+    <div className="content" style={{ minHeight: `100vh;` }}>
+      <div className="container">
         <h2>Latest News</h2>
-      </div> */}
-      {/* <NewsLatest /> */}
+      </div>
+      <NewsLatest />
+      <NewsSlider />
       <div className="section-1 container">
         <h3>Weather Forecast</h3>
         <Link to="/beaches">See all</Link>
