507ba2812260e569ce2d9d7ff0443b6fd0c43fdc
franciscocpereira <fmcp31@gmail.com>
2022-04-24 18:29:11 +0100

    Add font PT Root UI to project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Navbar from "./components/navigation/Navbar";
 import Footer from "./components/navigation/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/main/Main";
+import './fonts/fonts.css'
+
+// fonts 
+import './fonts/PT-Root-UI/PT-Root-UI_Bold.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Bold.woff2'
+import './fonts/PT-Root-UI/PT-Root-UI_Light.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Light.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Medium.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Medium.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Regular.woff'
+import './fonts/PT-Root-UI/PT-Root-UI_Regular.woff'
+
+
 function App() {
   return (
     <div className="App">
