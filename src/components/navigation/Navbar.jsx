b6f094479a3d5a311c7cb98eedb1df24174059b3
franciscocpereira <fmcp31@gmail.com>
2022-04-25 00:42:13 +0100

    Modify links to close mobileNav onClick

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -18,24 +18,24 @@ function Navbar(props) {
     return (
 
         <div className="navbar-container">
-            <Link to="/"><img id="logo" src={logo} alt="DuckDive"/></Link>
+            <Link to="/"><img id="logo" src={logo} onClick={handleHamburger} alt="DuckDive"/></Link>
             <nav className="navbar-desktop">
  
                     <div className="">
-                        <Link to="/">Home</Link>
-                        <Link to="/beaches">Beaches</Link>
-                        <Link to="/news">News</Link>
-                        <Link to="/info">Info</Link>
+                        <Link to="/" onClick={handleHamburger}>Home</Link>
+                        <Link to="/beaches" onClick={handleHamburger}>Beaches</Link>
+                        <Link to="/news" onClick={handleHamburger}>News</Link>
+                        <Link to="/info" onClick={handleHamburger}>Info</Link>
                     </div>        
             </nav>
             <button className={ active ? "burger-is-active , hamburger" : "hamburger"} onClick={handleHamburger}>
                 <div className="bar"></div>
             </button>
             <nav className={ active ? "side-is-active , mobileNav" : "mobileNav"} >
-                <Link to="/">Home</Link>
-                <Link to="/beaches">Beaches</Link>
-                <Link to="/news">News</Link>
-                <Link to="/info">Info</Link>
+                <Link to="/" onClick={handleHamburger}>Home</Link>
+                <Link to="/beaches" onClick={handleHamburger}>Beaches</Link>
+                <Link to="/news" onClick={handleHamburger}>News</Link>
+                <Link to="/info" onClick={handleHamburger}>Info</Link>
             </nav>
         </div>
     );
