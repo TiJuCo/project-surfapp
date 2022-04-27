5649c3ea78031696a30095034a37d4407c725fbb
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-04-27 19:45:28 +0100

    First fetch success

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./styles/styles.css";
 import Navbar from "./components/navigation/Navbar";
 import Footer from "./components/navigation/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApiContextProvider } from "./contexts/ApiContext";
 import Main from "./components/main/Main";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Main />
-      <Footer/>
+      <ApiContextProvider>
+        <Main />
+      </ApiContextProvider>
+      <Footer />
     </div>
   );
 }
--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const ApiContext = createContext([]);
 
@@ -6,6 +7,31 @@ export default ApiContext;
 
 export const ApiContextProvider = ({ children }) => {
   const [state, setState] = useState([]);
+
+  const getWeatherInfo = () => {
+    axios
+      .get("https://run.mocky.io/v3/53ef2996-2447-4c63-806f-f082b0dcea7a")
+      .then((response) => getIpmaInfo(response.data.districts));
+  };
+
+  const getIpmaInfo = (ourApi) => {
+    const results = [];
+    ourApi.map((x) =>
+      axios
+        .get(
+          `https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/${x.id}.json`
+        )
+        .then((response) => response.data)
+        .then((data) => {
+          results.push(data.data[0]);
+        })
+    );
+    setState(results);
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
 
   return (
     <ApiContext.Provider value={(state, setState)}>
