20ae3acedd72dd46f5b40766d352bb079d8eef6e
Juan Giménez <juanp.gi2485@gmail.com>
2022-04-29 09:59:02 +0100

    delete news section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import NewsLatest from "../news/News-Latest";
 import { Link } from "react-router-dom";
 import WeatherForecast from "../WeatherForecast/WeatherForecast";
 import "../../styles/styles.css";
 import "./home.css";
-import NewsSlider from "../news/News-Latest-Mobile";
+
 
 function Home() {
   return (
     <div className="content" style={{ minHeight: `100vh;` }}>
       <div className="section-1 container">
         <h3>Latest News</h3>
-        <Link to="/news">See all</Link>
+        
       </div>
-      <NewsLatest />
-      <NewsSlider />
-
+      
       <div className="section-2 container">
         <h3>Weather Forecast</h3>
         <Link to="/beaches">See all</Link>
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../home/Home";
-import NewsList from "../news/News-List";
+
 
 function Main() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/news" element={<NewsList />} /> */}
       {/* <Route path="/beaches" element={<Beaches />} /> */}
     </Routes>
   );
