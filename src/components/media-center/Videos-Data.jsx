859f121dc779b714d2621af71fc3a56a8fd9765a
Juan Giménez <juanp.gi2485@gmail.com>
2022-05-04 20:38:39 +0100

    fix carousel and add more videos

--- src/components/media-center/Videos-Data.jsx
+++ src/components/media-center/Videos-Data.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import surf1 from '../../media/surf1.mp4';
 import surf2 from '../../media/surf2.mp4';
 import surf3 from '../../media/surf3.mp4';
+import surf4 from '../../media/surf4.mp4';
+import surf5 from '../../media/surf5.mp4';
+import surf6 from '../../media/surf6.mp4';
+import surf7 from '../../media/surf7.mp4';
+import surf8 from '../../media/surf8.mp4';
+import surf9 from '../../media/surf9.mp4';
+import surf10 from '../../media/surf10.mp4';
 
 export const surfVideos = [
 
@@ -26,24 +33,53 @@ export const surfVideos = [
         video: surf3
     },
 
+
     {
         index: 3,
         tag: 'videos',
-        title: 'Carcavelos surf',
-        video: 'https://youtu.be/ret0l1eZRp8'
+        title:'Ride of the Year Award Entry ',
+        video: surf4
     },
 
     {
         index: 4,
         tag: 'videos',
-        title:'Ride of the Year Award Entry ',
-        video: 'https://youtu.be/NTgAqH9MEeg'
+        title:'Video of the week',
+        video: surf5
     },
 
     {
         index: 5,
         tag: 'videos',
-        title:'Ericeira - Video of the week',
-        video: 'https://youtu.be/tjRnLNrutUg'
+        title:'',
+        video: surf6
+    },
+
+    {
+        index: 6,
+        tag: 'videos',
+        title:'',
+        video: surf7
+    },
+
+    {
+        index: 7,
+        tag: 'videos',
+        title:'',
+        video: surf8
+    },
+
+    {
+        index: 8,
+        tag: 'videos',
+        title:'',
+        video: surf9
+    },
+
+    {
+        index: 9,
+        tag: 'videos',
+        title:'',
+        video: surf10
     }
 ]
