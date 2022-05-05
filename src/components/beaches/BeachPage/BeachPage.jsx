026f2e3f3ae660ba1e1432bac02bf73df0e94058
franciscocpereira <fmcp31@gmail.com>
2022-05-05 17:13:41 +0100

    Add conditions report text, current conditions, calculator card, initialized swiper for conditions by day

--- src/components/beaches/BeachPage/BeachPage.jsx
+++ src/components/beaches/BeachPage/BeachPage.jsx
@@ -2,6 +2,12 @@ import React, { useContext, useState } from "react";
 import './BeachPage.css'
 import { useParams } from "react-router-dom";
 import ApiContext from "../../../contexts/ApiContext";
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css/pagination";
+import "swiper/css";
 import {
     sol,
     swellDuration,
@@ -18,6 +24,10 @@ import {
     transportation,
     wc,
     accessibility,
+    arrowFullLeft,
+    beginnerMobile,
+    intermediateMobile,
+    advancedMobile,
   } from "../../media/exportMedia";
 
 
@@ -26,14 +36,26 @@ import {
 function BeachPage() {
 
     let params = useParams();
-
     
     const { seaInfo, beachesInfo } = useContext(ApiContext);
 
+    seaInfo.forEach((element, index) => {
+        element.name = beachesInfo[index].name;
+        element.county = beachesInfo[index].county;
+        element.img = beachesInfo[index].img;
+        element.services = beachesInfo[index].services;
+    });
+
     const gradient = "linear-gradient(360deg, rgba(57, 73, 87, 0.9) 0%, rgba(255, 255, 255, 0) 100%)";
 
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     const date = new Date();
     const [time] = useState(date.getHours());
+    const [day] = useState(date.getDate());
+    const [dateMonth] = useState(date.getDay());
+    
+    const d = new Date();
+    let month = months[d.getMonth()];
    
     return (
         <div className="container beach-page-container">
@@ -41,11 +63,12 @@ function BeachPage() {
             {seaInfo.filter((page, index) => page.name === params.beachName).map((page) => ( 
 
                 <div className="beach-page">    
-                
-                    <div>
-                        <img src="" alt="" />
-                        <p>All beaches</p>
-                    </div>
+                    <Link to={`/beaches/`}>
+                        <div className="back">
+                            <img src={arrowFullLeft} alt="back" />
+                            <p>All beaches</p>
+                        </div>
+                    </Link>
                     <div className="beach-page-card" style={{ backgroundImage: `url(${page.img}), ${gradient} ` }}>
                         <h1>{page.name}</h1>
                         <div>
@@ -54,14 +77,16 @@ function BeachPage() {
                         </div>
                     </div>
                     <div className="beach-page-content">
-                        <p>Today, {time > 12 ? `${time - 12} pm` : `${time} am`}</p>
+                        <div className="beach-page-time">
+                            <p className="beach-page-time-text"><span className="accent">Today</span>, {day} {month} <span class="dot-accent"></span> {time > 12 ? `${time - 12} pm` : `${time} am`}</p>
+                        </div>
 
                         <p className="beach-page-text-report">
                             We register a temperature of <span className="pageObject">{page.airTemperature.sg}º</span>, <span className="pageObject">{page.windSpeed.sg} kts</span> wind from the 
 
                             <span className="pageObject">{page.windDirection.sg <= 22.5 ? " North " : 
                                 page.windDirection.sg > 22.5 &&
-                                page.windDirection.sg <= 67.5 ? " North West " :
+                                page.windDirection.sg <= 67.5 ? " North East " :
                                 page.windDirection.sg > 67.5 &&
                                 page.windDirection.sg <= 112.5 ? " East " :
                                 page.windDirection.sg > 112.5 &&
@@ -79,16 +104,86 @@ function BeachPage() {
                             }</span>
                             , a wave height of  <span className="pageObject">{page.waveHeight.sg} m</span>, a swell interval of <span className="pageObject">{page.wavePeriod.noaa} kts</span>.
                         </p>
-                        <p className="news-page-text">
-                            {page.text}
-                        </p>
-                        <p className="news-page-text">
-                            {page.text}
+
+                        <p className="beach-page-text-report-tide">
+                            A preia-mar dá-se às <span className="pageObject">12:43</span>, <br/> a baixa-mar às <span className="pageObject">18:43</span>. 
                         </p>
                     </div>
-                    <div>
-                        <h3>Gallery</h3>
+                    <div className="beach-page-calculator">
+                        <div>
+                            <p>Excellent</p>
+                            <div className="calc-dots">
+                                <span class="dot-accent"></span>
+                                <span class="dot-accent"></span>
+                                <span class="dot-accent"></span>
+                                <span class="dot-accent"></span>
+                                <span class="dot-accent"></span>
+                            </div> 
+                        </div>
+                        <p>Today, {day} {month}</p>
+                        <div>
+                            <img src={beginnerMobile}alt="" />
+                            <img src={intermediateMobile}alt="" />
+                            <img src={advancedMobile}alt="" />
+                        </div>
+                          
                         
+                        
+                    </div>   
+                    <div className="current-conditions-container">
+                        <h3>Current conditions</h3>
+                        <div className="current-conditions">
+                            <div>
+                                <img src={sol} alt="" />
+                                <p>{parseInt(page.airTemperature.sg)}º</p>
+                            </div>
+                            <div>
+                                <img src={swellDuration} alt="" />
+                                <p>{parseFloat(page.wavePeriod.noaa).toFixed(1)}s</p>
+                            </div>
+                            <div>
+                                <img src={swellHeight} alt="" />
+                                <p>{parseFloat(page.waveHeight.sg).toFixed(1)}m</p>
+                            </div>
+                            <div>
+                                <img src={wind} alt="" />
+                                <p>
+                                {parseFloat(page.windSpeed.sg).toFixed(1)}<span className="kts">kts</span> 
+                                <span className="pageObject">{page.windDirection.sg <= 22.5 ? " N " : 
+                                page.windDirection.sg > 22.5 &&
+                                page.windDirection.sg <= 67.5 ? " NE " :
+                                page.windDirection.sg > 67.5 &&
+                                page.windDirection.sg <= 112.5 ? " E " :
+                                page.windDirection.sg > 112.5 &&
+                                page.windDirection.sg <= 157.5 ? " SE " :
+                                page.windDirection.sg > 157.5 &&
+                                page.windDirection.sg <= 202.5 ? " S " :
+                                page.windDirection.sg > 202.5 &&
+                                page.windDirection.sg <= 247.5 ? " SW " :
+                                page.windDirection.sg > 247.5 &&
+                                page.windDirection.sg <= 292.5 ? " W " :
+                                page.windDirection.sg > 292.5 &&
+                                page.windDirection.sg <= 337.5 ? " NW " :
+                                page.windDirection.sg > 337.5 &&
+                                page.windDirection.sg <= 360 ? " N " : "null"  
+                            }</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>              
+                    <div className="conditions-hour-container">
+                        <h3>Current conditions</h3>
+                        <div className="conditions-hour">
+                            {/* <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                                {page.map((element, index) => (
+                                <SwiperSlide key={seaInfo.index}>
+                                    <div>
+                                        Today 
+                                    </div>
+                                </SwiperSlide>
+                                ))}
+                            </Swiper> */}
+                        </div>
                     </div>              
                 </div>
             ))}
--- src/components/media/exportMedia.jsx
+++ src/components/media/exportMedia.jsx
@@ -1,6 +1,7 @@
 export const sol = require("./sol.svg");
 export const arrowUp = require("./arrowUp.svg");
 export const arrowDown = require("./arrowDown.svg");
+export const arrowFullLeft = require("./arrowFullLeft.svg");
 export const swellDuration = require("./swellDuration.svg");
 export const swellHeight = require("./swellHeight.svg");
 export const wind = require("./wind.svg");
@@ -20,3 +21,7 @@ export const wc = require("./wc.svg");
 export const accessibility = require("./accessibility.svg");
 
 
+// levels 
+export const beginnerMobile = require("./beginnerMobile.svg");
+export const intermediateMobile = require("./intermediateMobile.svg");
+export const advancedMobile = require("./advancedMobile.svg");
