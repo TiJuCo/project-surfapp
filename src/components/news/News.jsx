35e0e1c2347833e06146b4875d2abe77790cf602
franciscocpereira <fmcp31@gmail.com>
2022-05-07 13:08:10 +0100

    Fixed News main page css and structure, desktop and mobile

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -2,13 +2,31 @@ import React from "react";
 import "./News.css";
 import { ArticlesData } from "./NewsData";
 import NewsCards from "./NewsCards";
+import NewsLatestSlider from "./NewsLatestSlider"
 
 function News() {
   return (
-    <div className="news-card-container">
-      {ArticlesData.filter((info) => info.tag === "news").map((info) => (
-        <NewsCards {...info} />
-      ))}
+
+    <div className="content">
+
+        <div className="section section-1 container">
+          <h3>Popular News</h3>
+        </div>
+        <div className="news-card-container container">
+          {ArticlesData.filter((info) => info.tag === "news").map((info) => (
+            <NewsCards {...info} />
+          ))}
+        </div>
+        <NewsLatestSlider/>
+
+        <div className="section section-1 container">
+          <h3>All News</h3>
+        </div>
+        <div className="news-list-container container">
+          {ArticlesData.filter((info) => info.tag === "news").map((info) => (
+            <NewsCards {...info} />
+          ))}
+        </div>
     </div>
   );
 }
