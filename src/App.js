e5e20b27fb5685c6a46b4c0a783030d20a6adccc
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-05-07 22:56:58 +0100

    Erased the Calculator component. Moved into APIContext

--- src/App.js
+++ src/App.js
@@ -4,30 +4,28 @@ import Footer from "./components/navigation/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApiContextProvider } from "./contexts/ApiContext";
 import Main from "./components/main/Main";
-import './fonts/fonts.css'
-import Calculator from "./components/calculator/Calculator";
-// fonts 
-import './fonts/PT-Root-UI/PT-Root-UI_Bold.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Bold.woff2'
-import './fonts/PT-Root-UI/PT-Root-UI_Light.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Light.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Medium.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Medium.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Regular.woff'
-import './fonts/PT-Root-UI/PT-Root-UI_Regular.woff'
+import "./fonts/fonts.css";
+// fonts
+import "./fonts/PT-Root-UI/PT-Root-UI_Bold.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Bold.woff2";
+import "./fonts/PT-Root-UI/PT-Root-UI_Light.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Light.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Medium.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Medium.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Regular.woff";
+import "./fonts/PT-Root-UI/PT-Root-UI_Regular.woff";
 import Videos from "./components/media-center/Videosl";
 import Carousel from "./components/media-center/Carousel";
 import CarouselMobile from "./components/media-center/CarouselMobile";
 import NewsPage from "./components/news/NewsPage";
 
-
 function App() {
   return (
     <div className="App">
       <Navbar />
-        <ApiContextProvider>
-          <Calculator />
-        </ApiContextProvider>
+      <ApiContextProvider>
+        <Main />
+      </ApiContextProvider>
       <Footer />
     </div>
   );
